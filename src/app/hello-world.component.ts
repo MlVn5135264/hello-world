d704873382c808ac6fb6b4ff7141455be4c9383a
MlVn5135264 <MlVn5135264.yahoo.com>
2023-08-08 17:51:43 -0700

    using functions to return values to view

--- src/app/hello-world.component.ts
+++ src/app/hello-world.component.ts
@@ -7,5 +7,9 @@ import { Component } from "@angular/core";
 })
 
 export class helloWorldComponent{
-    name = "Test"
+    name = 'Wecome to the Angular World Mohan'
+    getValue()
+    {
+        return this.name
+    }
 }
